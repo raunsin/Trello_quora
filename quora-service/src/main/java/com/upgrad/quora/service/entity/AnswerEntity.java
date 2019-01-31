a6e2aed6750cf7e274811433505e41c042e901f9
GhanDeveloper <GDg11104@gmail.com>
2019-01-31 21:16:18 +0530

    Initial Answer Controller/Model/entity

---
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/AnswerEntity.java
@@ -0,0 +1,85 @@
+package com.upgrad.quora.service.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "answer" , schema = "quora")
+public class AnswerEntity implements Serializable {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "UUID")
+    @NotNull
+    private String uuid;
+
+    @Column(name = "ANS")
+    @NotNull
+    private String answer;
+
+    @Column(name = "DATE")
+    @NotNull
+    private ZonedDateTime date;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "QUESTION_ID")
+    private QuestionEntity question;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public ZonedDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(ZonedDateTime date) {
+        this.date = date;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public QuestionEntity getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(QuestionEntity question) {
+        this.question = question;
+    }
+}
